a427503f58250614f175fb8c66f8644cc586a7a6
kbnestor <koh.beng.chuah@nestor.com.my>
2015-12-01 10:55:37 +0800

    aladdin_gh2_novendor
    aladdin_gh2_novendor

---
+++ backend/config/main-local.php
@@ -0,0 +1,25 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => '33P1sAs0bi_dyB2Ib6ZnNbtkUQ2W_Ibs',
+        ],
+    ],
+];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
+}
+
+return $config;
---
+++ frontend/views/customers/add.php
@@ -0,0 +1,28 @@
+<?php
+use frontend\models\customer\CustomerRecord;
+use frontend\models\customer\PhoneRecord;
+use yii\web\View;
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/**
+ * Add Customer UI.
+ *
+ * @var View $this
+ * @var CustomerRecord $customer
+ * @var PhoneRecord $phone
+ */
+
+$form = ActiveForm::begin([
+    'id' => 'add-customer-form',
+]);
+
+echo $form->errorSummary([$customer, $phone]);
+echo $form->field($customer, 'name');
+echo $form->field($customer, 'birth_date');
+echo $form->field($customer, 'notes');
+
+echo $form->field($phone, 'number');
+
+echo Html::submitButton('Submit', ['class' => 'btn btn-primary']);
+ActiveForm::end();
